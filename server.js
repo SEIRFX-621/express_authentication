09623e32a2c87c54c6dd7cb54484954a8977224d
connorGA <ronnoccap@gmail.com>
2022-09-13 20:13:05 -0700

    staged changes

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('./config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn');
 
-// console.log(SECRET_SESSION);
+const SECRET_SESSION = process.env.SECRET_SESSION;
+console.log('yoooooooo.....', SECRET_SESSION);
+
 
 app.set('view engine', 'ejs');
 
@@ -12,10 +18,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+app.use(flash());            // flash middleware
+
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
+
+app.use((req, res, next) => {
+  console.log('res locals >>>', res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+})
+
 app.get('/', (req, res) => {
   res.render('index');
 })
 
+//access to all of our auth routes GET /auth/login, GET /auth/signup, POST routes
+app.use('/auth', require('./controllers/auth'));
+
+// Add this above /auth controllers
+app.get('/profile', isLoggedIn, (req, res) => {
+  const { id, name, email } = req.user.get(); 
+  res.render('profile', { id, name, email });
+});
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
